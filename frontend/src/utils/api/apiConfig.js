d66109ab7c767ad7f3493ac94000fa689ab0a7a1
Kate Leonchikova <leonchikova.kate@gmail.com>
2025-01-19 16:58:37 +0100

    bugs fixed

--- frontend/src/utils/api/apiConfig.js
+++ frontend/src/utils/api/apiConfig.js
@@ -1,5 +1,5 @@
 // API URL
-export const API_URL = process.env.API_URL || 'http://localhost:3000';
+export const API_URL = process.env.WEBPACK_API_URL || 'http://localhost:3000';
 
 // WebSocket URL
-export const WS_URL = process.env.WS_URL || 'ws://localhost:3000';
+export const WS_URL = process.env.WEBPACK_WS_URL || 'ws://localhost:3000';
